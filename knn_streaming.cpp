ba03aee64cadc749ab196b67e065582859ab1ce5
ceys <zcnature@126.com>
2013-04-22 18:24:36 +0800

    'fuck'

--- knn_streaming.cpp
+++ knn_streaming.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <set>
-#include <map>
 #include <fstream>
 #include <sstream>
 #include <cmath>
@@ -12,6 +10,16 @@ using namespace std;
 struct sample {
     string type;
     vector<int> features;
+};
+
+struct output {
+    string id;
+    string type;
+};
+
+struct typeDistance {
+    string type;
+    float distance;
 };
 
 
@@ -47,10 +55,12 @@ void readTrain(vector<sample>& train, const string& file) {
         while (sin >> d) {
             ts.features.push_back(d);
         }
+        sort(ts.begin(), ts.end());
         train.push_back(ts);
     }
     fin.close();
 }
+
 
 //ToDo:可以考虑在这个函数加入对特征排序的选项
 vector<int> split_to_int(string& str, const string& pattern) {
@@ -70,10 +80,50 @@ vector<int> split_to_int(string& str, const string& pattern) {
 }
 
 
-void knnProcess(const vector<sample>& train, const string pattern) {
+bool comp(typeDistance& t1, typeDistance& t2) {
+    return t1.distance < t2.distance ? true : false;
+}
+
+string classify(const vector<sample>& train, vector<int>& test, string& id) {
+    float distance;
+    typeDistance td;
+    td.id = "";
+    td.distance = 0.0;
+    vector<typeDistance> topN(td, 11);
+    make_heap(topN.begin(), topN.end(), comp);
+    for (vector<sample>::iterator iter = train.begin(); iter != train.end(); ++iter) {
+        distance = jaccard_distance(*iter.features, &test);
+        if (topN.frout().distance < distance) {
+            pop_heap(topN.begin(),topN.end());
+            topN.pop_back();
+            typeDistance intd;
+            intd.id = *id;
+            intd.distance = distance;
+            topN.push_back(intd);
+            push_heap(topN.begin(),topN.end());
+        }
+    }
+    map<string, int> countmap;
+    for (vector<typeDistance>::iterater iter = topN.begin(); iter != topN.end(); ++iter) {
+        countMap
+
+
+void knn_process(const vector<sample>& train, const string pattern) {
     string line;
-    while (cin >> line) { 
+    int d = 0;
+    vector<int> test;
+    while (getline(cin, line)) {
+        istringstream sin(line);
+        output out;
+        sin >> out.id;
+        while (sin >> d) {
+            test.push_back(d);
+        }
+        sort(test.begin(), test.end());
+        out.type = classify(train, &test)
         
+    }
+       
 
 int main() {
     int va1[] = {1,2,3,4,5,6};
@@ -82,5 +132,10 @@ int main() {
     vector<int> v2(va2,va2+5); 
     float x = jaccard_distance(v1,v2);
     cout << x << endl;
+    
+    int test;
+    while (cin >> test) {
+        cout << test;
+    }
     return 0;
 }
